8bcbc896a5ee9222c1524ee067080f1c60e8e58d
drindustrial <d23den1@yandex.ru>
2020-02-12 16:05:07 +0700

    Create dataprep.py

---
+++ dataprep.py
@@ -0,0 +1,16 @@
+def read_osu(name):
+    
+    return 0
+
+
+
+def read_music(name):
+    return 0
+def convert_osu():
+    return 0
+def convert_music():
+    return 0
+def make_dataset():
+    return 0
+def get_files_list():
+    return 0
