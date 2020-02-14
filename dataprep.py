c5f7cc7539baa737cc4f254a9f9cc99a8491c087
drindustrial <d23den1@yandex.ru>
2020-02-14 16:40:30 +0700

    Update dataprep.py

--- dataprep.py
+++ dataprep.py
@@ -1,21 +1,45 @@
+import math as m
 def read_osu(name):
     f = open(name, 'r')
-    print(f.readline())
     osufile = []
     for line in f:
         osufile.append(line)
-        print(line)
+    omap = osumap(osufile)
     print(osufile[105])
     version = 0
     return 0
 
-class key():
+class note():
     key = 0
     time = 0
-    def __init__(self,key,time):
-        self.key = key
-        self.time = time
+    length = 0
+    def __init__(self,key,time,leng):
+        self.key = int(key)
+        self.time = int(time)
+        self.length = int(leng)
         pass
+    def printall(self):
+        print("key = ", self.key,"time = ", self.time,"length = ",self.length)
+    
+    
+    
+class hitobject():
+    x = 0
+    y = 0
+    time = 0
+    typeo = 0
+    hitSound = 0
+    endTime = 0
+    hitSample = ''
+    def __init__(self,x,y,time,typeo,hitSound,endTime,hitSample):
+        self.x = int(x)
+        self.y = int(y)
+        self.time = int(time)
+        self.typeo = int(typeo)
+        self.hitSound = int(hitSound)
+        self.endTime = int(endTime)
+        self.hitSample = hitSample
+        pass    
     
 
 class osumap():
@@ -39,12 +63,13 @@ class osumap():
     SpecialStyle = 0   #0 or 1	Whether or not the "N+1" style key layout is used for osu!mania	0
     WidescreenStoryboard = 0   #0 or 1	Whether or not the storyboard allows widescreen viewing	0
     SamplesMatchPlaybackRate = 0   #0 or 1	Whether or not sound samples will change rate when playing with speed-changing mods
+    
     #[Editor]	Saved settings for the beatmap editor
-    Bookmarks  #Comma-separated list of integers	Time in milliseconds of bookmarks
-    DistanceSpacing  #Decimal	Distance snap multiplier
-    BeatDivisor  #Decimal	Beat snap divisor
-    GridSize  #Integer	Grid size
-    TimelineZoom  #Decimal	Scale factor for the object timeline
+    Bookmarks  = ''  #Comma-separated list of integers	Time in milliseconds of bookmarks
+    DistanceSpacing = 0  #Decimal	Distance snap multiplier
+    BeatDivisor = 0  #Decimal	Beat snap divisor
+    GridSize = 0  #Integer	Grid size
+    TimelineZoom = 0  #Decimal	Scale factor for the object timeline
     
     #[Metadata]	Information used to identify the beatmap
     Title = ''   #String	Romanised song title
@@ -57,6 +82,7 @@ class osumap():
     Tags = ''   #Space-separated list of strings	Search terms
     BeatmapID = 0    #Integer	Beatmap ID
     BeatmapSetID = 0    #Integer	Beatmapset ID
+    
     #[Difficulty]	Difficulty settings
     HPDrainRate = 0    #Decimal	HP setting (0–10)
     CircleSize = 0    #Decimal	CS setting (0–10)
@@ -73,8 +99,49 @@ class osumap():
     colours = []
     #[HitObjects]
     hitobjects = []
-    def __init__(self):
-        """Constructor"""
+    notes = []
+    bpm = 0
+    offset = 0
+    version = 0
+    def __init__(self,file):
+        for i in range(len(file)):
+            cur = file[i]
+            print(cur)
+            if(cur[:3] == "osu"):
+                self.version = int(cur[-3:-1])
+            if(cur == "[General]\n"):
+                print("ok")
+            if(cur == "[Editor]\n"):
+                print("ok")
+            if(cur == "[Metadata]\n"):
+                print("ok")
+            if(cur == "[Difficulty]\n"):
+                print("ok")
+            if(cur == "[Events]\n"):
+                print("ok")
+            if(cur == "[TimingPoints]\n"):
+                print("ok")
+            if(cur == "[Colours]\n"):
+                print("ok")
+            if(cur == "[HitObjects]\n"):
+                for j in range(i+1,len(file)):
+                    cur = file[j]
+                    hit = hitobject(cur.split(',')[0],
+                                    cur.split(',')[1],
+                                    cur.split(',')[2],
+                                    cur.split(',')[3],
+                                    cur.split(',')[4],
+                                    cur.split(',')[5].split(':')[0],
+                                    cur.split(',')[5])
+                    if(hit.typeo == 128):
+                        n = note(m.floor(hit.x * self.CircleSize / 512),hit.time,hit.endTime - hit.time)
+                    else:
+                        n = note(m.floor(hit.x * self.CircleSize / 512),hit.time,0)
+                    self.hitobjects.append(hit)
+                    self.notes.append(n)
+                    n.printall()
+        
+            
         
 
 
